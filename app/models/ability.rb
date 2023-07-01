a174fbfc1be6f7667a94d4583f04fd351d7363e7
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-07-01 10:05:50 +0530

    FIX:new post issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,19 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user
-    can :read, Post, public: true
+    user ||= User.new 
 
-    return unless user.present?
-
-    can :read, Post, user_id: user.id
-    can :destroy, Post, author_id: user.id
-    can :destroy, Comment, author_id: user.id
-    can :destroy, Post, id: user.posts.pluck(:id)
-
-    return unless user.admin?
-
-    can :manage, Post
-    can :manage, Comment
-  end
+    if user.admin? 
+    can :manage, :all
+    else
+    can :manage, Post, author: user
+    can :manage, Comment, author: user
+    can :read, :all
+    end
+end
 end
